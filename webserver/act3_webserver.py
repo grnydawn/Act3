72ce05e4f03f72f2b8852fa8e7ccf40e305379f9
Youngsung Kim <youngsun@ucar.edu>
2016-01-14 23:22:39 -0700

    created portal pages

--- webserver/act3_webserver.py
+++ webserver/act3_webserver.py
@@ -1,9 +1,39 @@
+import os
 import cherrypy
-      
-class HelloWorld(object):
+
+HTML_HOME = os.path.dirname(os.path.realpath(__file__))
+
+class A3WebService(object):
     def index(self):
-        return "Hello World!"
+        with open('%s/index.html'%HTML_HOME, 'r') as fd:
+            return fd.read()
     index.exposed = True
 
 def start():
-    cherrypy.quickstart(HelloWorld())
+
+    conf = {
+        '/': {
+            'tools.sessions.on': True,
+            'tools.staticdir.root': HTML_HOME
+        },
+        '/static': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': './public'
+        }
+    }
+
+    webapp = A3WebService()
+
+    cherrypy.tree.mount(webapp, '/', conf)
+
+    #ns = Pyro4.locateNS()
+    #order_mgr = Pyro4.Proxy("PYRONAME:kvec.order_mgr")
+
+    cherrypy.engine.start()
+    cherrypy.engine.block()
+
+def stop():
+    cherrypy.engine.exit()
+
+if __name__ == '__main__':
+    start()
